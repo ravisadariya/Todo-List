beb93a7747ffd54bb2ad802cac6a7c728e6a369e
Ravi Sadariya <ravisadariya03@gmail.com>
2022-03-09 17:49:51 +0530

    Final Project

---
+++ src/MyComponents/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const About = () => {
+    return (
+        <div>
+            This is an About Component
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, cumque
+                cupiditate expedita consequatur commodi eligendi sint soluta pariatur
+                aliquam assumenda, voluptatibus, distinctio minus asperiores ducimus
+                temporibus tempora odio officiis dicta necessitatibus aspernatur
+                delectus voluptates.
+            </p>
+        </div>
+    )
+}
